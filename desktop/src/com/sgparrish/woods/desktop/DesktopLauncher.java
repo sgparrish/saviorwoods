a0baea71d3aea6cff13c89f0b11c5ca186599b09
sgparrish <sgparrish334@gmail.com>
2016-01-09 22:52:39 -0500

    Initial commit

---
+++ desktop/src/com/sgparrish/woods/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sgparrish.woods.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sgparrish.woods.WoodsMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new WoodsMain(), config);
+	}
+}
